fd81debcc6a3bd98ee782c2903e46435981e6120
SWTT <cgrimward.dwp@gmail.com>
2015-06-02 15:02:52 +0100

    Java doc for DataAccess interface

--- src/fortress/bid/interfaces/IDataAccess.java
+++ src/fortress/bid/interfaces/IDataAccess.java
@@ -5,22 +5,61 @@ import fortress.bid.*;
 public interface IDataAccess {
 	
 
+	/**
+	 * Adds a new listing to the database
+	 * @param newListing the listing to be inserted
+	 */
 	public abstract void insertListing(Listing newListing);
 	
+	/**
+	 * Gets all of a users listings in which they are acting as the seller that are currently in progress and have not yet sold or expired
+	 * @param userID id of the user currently logged in
+	 */
 	public abstract void getListingInProgress(int userID);
 	
+	/**
+	 * Gets all of the users listings where they are were acting as the seller which have successfully sold (time has expired and reservation amount was exceeded) 
+	 * @param userID id of the user currently logged in
+	 */
 	public abstract void getListingSold (int userID);
 	
+	/**
+	 * removes listings which are no longer in progress and have expired (no buyer found for the listing)
+	 * @param listingIDs 1 or more listingID's to be removed from the auctions
+	 */
 	public abstract void removeListings (int [] listingIDs );
 	
+	/**
+	 * searches for all listings where the searchTerm parameter is applicable
+	 * @param searchTerm string to search listings for
+	 */
 	public abstract void searchListings (String searchTerm);
 	
+	
+	/**
+	 * Inserts a bid
+	 * @param listingID listing to bid against
+	 * @param userID user creating the bid
+	 * @param bidAmount the amount to bid against the item
+	 */
 	public abstract void insertBid (int listingID, int userID, double bidAmount);
 	
+	/**
+	 * Gets all listings where the user has won the auction
+	 * @param userID user currently logged in
+	 */
 	public abstract void getListingsPurchased (int userID);
 	
+	/**
+	 * adds a new user into the database
+	 * @param newUser the user to be registered
+	 */
 	public abstract void insertUser (User newUser);
 	
+	/**
+	 * gets all information about a user
+	 * @param usernameEntered username of the user to retrieve information
+	 */
 	public abstract void getUserInfo (String usernameEntered);
 
 }
