971c2e6ffbf256c944faa1662ce482e1a5bd3c58
unknown <10071638@LDSLT20.lds.ecomms.local>
2015-06-02 14:14:22 +0100

    completed iDataAccess interface

--- src/fortress/bid/interfaces/IDataAccess.java
+++ src/fortress/bid/interfaces/IDataAccess.java
@@ -1,5 +1,23 @@
 package fortress.bid.interfaces;
 
 public interface IDataAccess {
+	
+	public abstract void insertListing(Listing newListing);
+	
+	public abstract void getListingInProgress(int userID);
+	
+	public abstract void getListingSold (int userID);
+	
+	public abstract void removeListings (int [] listingIDs );
+	
+	public abstract void searchListings (String searchTerm);
+	
+	public abstract void insertBid (int listingID, int userID, double bidAmount);
+	
+	public abstract void getListingsPurchased (int userID);
+	
+	public abstract void insertUser (User newUser);
+	
+	public abstract void getUserInfo (String usernameEntered);
 
 }
