afde7d4e295e6b4ae6ce9c88119817f0b3e09bd7
unknown <10071638@LDSLT20.lds.ecomms.local>
2015-06-02 14:54:56 +0100

    Completed auction service interface

---
+++ src/fortress/bid/interfaces/IAuctionService.java
@@ -0,0 +1,27 @@
+package fortress.bid.interfaces;
+
+import java.util.ArrayList;
+
+import fortress.bid.*;
+
+public interface IAuctionService {
+	
+	public abstract boolean validateUser (String usernameEntered, String passwordEntered);
+	
+	public abstract User getListingsInProgress (User currentUser);
+	
+	public abstract User getListingsSold (User currentUser);
+	
+	public abstract User getListingsExpired (User currentUser);
+	
+	public abstract User getListingsPurchased (User currentUser);
+	
+	public abstract void placeBid (int userID, int listingID, double bidAmount);
+	
+	public abstract ArrayList<Listing> searchForListing (String searchTerm);
+	
+	public abstract void registerUser (String username, String password, String email);
+	
+	public abstract void removeListings (int[] listingIDs);
+
+}
